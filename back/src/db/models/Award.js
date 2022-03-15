a074570d76b90f789e0c975955030741a7b5898b
YunSeongJun <movomo@gmail.com>
2022-03-15 17:21:59 +0900

    Create Award model skeleton

--- back/src/db/models/Award.js
+++ back/src/db/models/Award.js
@@ -1,37 +1,49 @@
 import { AwardModel } from "../schemas/award";
 
+/** Static container class for award model.
+ * 
+ * @class
+ */
 class Award {
   static async create({ newAward }) {
-    const createdNewAward = await AwardModel.create(newAward);
-    return createdNewAward;
-  }
-
-  static async findByEmail({ email }) {
-    const award = await AwardModel.findOne({ email });
+    const award = await AwardModel.create(newAward);
     return award;
   }
 
   static async findById({ award_id }) {
-    const award = await AwardModel.findOne({ id: award_id });
-    return award;
+
   }
 
-  static async findAll() {
-    const awards = await AwardModel.find({});
-    return awards;
+  static async findByName({ name }) {
+
+  }
+
+  static async searchByName({ name }) {
+    // const award = await AwardModel.findOne({ email });
+    // return award;
+  }
+
+  static async searchByDescription({ keyword }) {
+    // const award = await AwardModel.findOne({ id: award_id });
+    // return award;
+  }
+
+  static async searchByAwardee({ user }) {
+    // const awards = await AwardModel.find({});
+    // return awards;
   }
 
   static async update({ award_id, fieldToUpdate, newValue }) {
-    const filter = { id: award_id };
-    const update = { [fieldToUpdate]: newValue };
-    const option = { returnOriginal: false };
+    // const filter = { id: award_id };
+    // const update = { [fieldToUpdate]: newValue };
+    // const option = { returnOriginal: false };
 
-    const updatedUser = await AwardModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return updatedAward;
+    // const updatedUser = await AwardModel.findOneAndUpdate(
+    //   filter,
+    //   update,
+    //   option
+    // );
+    // return updatedAward;
   }
 }
 
