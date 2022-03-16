091b97ccc375fbef28166bf6cbfc8a87eb9943e9
YunSeongJun <movomo@gmail.com>
2022-03-16 13:37:49 +0900

    Fix 'name' filed to 'title'

--- back/src/db/schemas/award.js
+++ back/src/db/schemas/award.js
@@ -1,30 +1,30 @@
 import { Schema, model } from "mongoose";
 
 /** Schema representing an individual award.
- * 
- * @field {String} name
+ *
+ * @field {String} title
  * @field {String} description
- * @field {User} awardee
+ * @field {user} awardee
  **/
 const AwardSchema = new Schema(
     {
         id: {
             type: String,
-            required: true
+            required: true,
         },
-        name: {
+        title: {
             type: String,
-            required: true
+            required: true,
         },
         description: {
             type: String,
             required: false,
-            default: "설명이 아직 없습니다. 추가해 주세요."
+            default: "설명이 아직 없습니다. 추가해 주세요.",
         },
         awardee: {
             type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
+            ref: "User",
+        },
     },
     {
         timestamps: false,
