69b74bb43832fe9fecae98bdcbacd31d9ede08f1
handwoong <handwoong@gmail.com>
2022-03-16 17:42:56 +0900

    feat. 포트폴리오 소유자 확인
    포트폴리오 소유자를 확인하여 소유자가 아니라면 자격증 추가 버튼 비활성화

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -62,7 +62,7 @@ function Portfolio() {
           />
         </Col>
         <Col>
-          <CertificateCard />
+          <CertificateCard portfolioOwnerId={portfolioOwner.id} />
         </Col>
       </Row>
     </Container>
--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
+import { UserStateContext } from "../../App";
 import CertificateAddForm from "./CertificateAddForm";
 
-function CertificateCard() {
+function CertificateCard({ portfolioOwnerId }) {
+  const [isOwner, setIsOwner] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const userState = useContext(UserStateContext);
+
+  /**
+   * 포트폴리오의 주인인지 확인하여 isOwner의 상태 변경
+   * @param { string } userState.user.id - 로그인 되어있는 유저의 id
+   * @param { string } portfolioOwnerId - 엔드포인트 params :id
+   * **/
+  useEffect(() => {
+    if (userState.user.id === portfolioOwnerId) {
+      setIsOwner(true);
+      return;
+    }
+    setIsOwner(false);
+  }, [userState.user.id, portfolioOwnerId]);
 
   return (
     <Card>
@@ -15,9 +31,11 @@ function CertificateCard() {
           <br />
           날짜
         </Card.Text>
-        <Button variant="primary" onClick={() => setIsEditing(true)}>
-          +
-        </Button>
+        {isOwner && (
+          <Button variant="primary" onClick={() => setIsEditing(true)}>
+            +
+          </Button>
+        )}
         {isEditing && <CertificateAddForm setIsEditing={setIsEditing} />}
       </Card.Body>
     </Card>
