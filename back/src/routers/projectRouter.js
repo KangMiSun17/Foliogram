f26f27594c0c846805fc500af0223fc2582d3519
YunSeongJun <movomo@gmail.com>
2022-03-18 00:04:01 +0900

    Implement post, single get and put

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -5,4 +5,182 @@ import { projectService } from "../services/projectService";
 
 const projectRouter = Router();
 
+/** /project/create post router
+ *
+ * @implements POST /project/create
+ * @returns {project} created
+ *
+ * headers = { "authorization": bearer }
+ * body = {
+ *  "user_id",
+ *  "title",
+ *  "from_date",
+ *  ["to_date",]
+ *  ["description",]
+ * }
+ *
+ * Sample req.body = {
+ *  "user_id":"af4ff0af-2a5f-4eea-99f2-d18b42aba419",
+ *  "title":"react 프로젝트",
+ *  "description":"프론트엔드 역량을 키웠습니다!",
+ *  "from_date":"2021-03-20",
+ *  "to_date":"2021-04-20"
+}
+ */
+projectRouter.post(
+    "/project/create",
+    login_required,
+    async function (req, res, next) {
+        try {
+            if (is.emptyObject(req.body)) {
+                throw new Error(
+                    "headers의 Content-Type을 application/json으로 설정해주세요"
+                );
+            }
+            console.log(`POST /project/create req.body=`, req.body);
+
+            // These fields are required.
+            ["user_id", "title", "from_date"].forEach((k) => {
+                if (!(k in req.body)) {
+                    throw new Error(`${k} field is required`);
+                }
+            });
+
+            if (req.body.user_id !== req.currentUserId) {
+                throw new Error("Trying to create different user's project");
+            }
+
+            const payload = { ...req.body };
+            payload.participant_id = payload.user_id;
+            delete payload.user_id;
+
+            const newProject = await projectService.addProject(payload);
+
+            res.status(201).json(newProject);
+        } catch (why) {
+            next(why);
+        }
+    }
+);
+
+/** /projects/:id get router
+ *
+ * @implements GET /projects/:id
+ * @returns {project|null} project
+ */
+projectRouter.get(
+    "/projects/:id",
+    login_required,
+    async function (req, res, next) {
+        // Here we get the id of a project and give back a single project.
+        // It's possible to try first by exact name and retry with the id,
+        // but it's a bit awkward.
+        // On reflection, writing findByTitle wasn't all that smart after all.
+        try {
+            const project_id = req.params.id;
+            console.log(`GET /projects/${project_id}`);
+            const found = await projectService.getProject({ project_id });
+            res.status(200).json(found);
+        } catch (why) {
+            next(why);
+        }
+    }
+);
+
+/** /projects/:id put router
+ *
+ * @implements PUT /projects/:id
+ * @returns {project} updated
+ *
+ * Sample body = {
+ *  "title":"express 프로젝트",
+ *  "description":"프론트엔드 말고 백엔드 역량을 키웠습니다!",
+ *  "from_date":"2021-05-20",
+ *  "to_date":"2021-06-20"
+}
+ */
+projectRouter.put(
+    "/projects/:id",
+    login_required,
+    async function (req, res, next) {
+        try {
+            const project_id = req.params.id;
+            const user_id = req.currentUserId;
+
+            const project = await projectService.getProject({ project_id });
+            if (!project) {
+                throw new Error(`Project id ${project_id} not found`);
+            } else if (project.participant_id !== user_id) {
+                throw new Error(
+                    `User is not an owner of the project id ${project_id}`
+                );
+            }
+
+            console.log(
+                `PUT /projects/:${project_id},
+user_id=${user_id},
+participant_id=${project.participant_id}`
+            );
+            console.log(req.body);
+
+            const updated = await projectService.setProject({
+                project_id,
+                pairs: Object.entries(req.body),
+            });
+
+            res.status(200).json(updated);
+        } catch (why) {
+            next(why);
+        }
+    }
+);
+
+/** /projectlist/:user_id get router
+ *
+ * @implements GET /projectlist/:user_id
+ * @returns {[project]} projects
+ */
+projectRouter.get(
+    "/projectlist/:user_id",
+    login_required,
+    async function (req, res, next) {
+        try {
+            const user_id = req.params.user_id;
+            const projects = await projectService.getUserAwards({
+                projectee_id: user_id,
+            });
+
+            res.status(200).json(projects);
+        } catch (why) {
+            next(why);
+        }
+    }
+);
+
+/** /projects/:id delete router
+ *
+ * @implements DELETE /projects/:id
+ * @returns {Object} payload
+ * @returns {Boolean} payload.result
+ *
+ * @todo The control layer must respond as {result: true/false}.
+ */
+projectRouter.delete(
+    "/projects/:id",
+    login_required,
+    async function (req, res, next) {
+        try {
+            const project_id = req.params.id;
+
+            console.log(`DELETE /projects/:${project_id}`);
+
+            const deleted = await projectService.removeProject({ project_id });
+
+            res.status(deleted ? 200 : 500).json({ result: !!deleted });
+        } catch (why) {
+            next(why);
+        }
+    }
+);
+
 export { projectRouter };
--- back/src/services/projectService.js
+++ back/src/services/projectService.js
@@ -1,6 +1,179 @@
 import { Project } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
-class projectService {}
+/** Static container class for services related to projects.
+ *
+ * @class
+ * @method static async addProject({
+ *      participant_id,
+ *      title,
+ *      from_date,
+ *      to_date,
+ *      description,
+ *  })
+ * @method static async getProject({ id, title })
+ * @method static async getAllProjects()
+ * @method static async getUserProjects({ participant_id })
+ * @method static async searchProjects({ title, description })
+ * @method static async setProject({ project_id, pairs })
+ * @method static async removeProject({ project_id })
+ */
+class projectService {
+    /** Add a project to the user.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {uuid} payload.participant_id
+     * @param {String} payload.title
+     * @param {String} payload.from_date - YYYY-MM-DD
+     * @param {String} [payload.to_date] - YYYY-MM-DD
+     * @param {String} [payload.description]
+     * @returns {project} added
+     */
+    static async addProject({
+        participant_id,
+        title,
+        from_date,
+        to_date,
+        description,
+    }) {
+        const allowedFields = [
+            "participant_id",
+            "title",
+            "from_date",
+            "to_date",
+            "description",
+        ];
+        const newProject = Object.fromEntries(
+            Object.entries(arguments[0]).filter(([k, v]) =>
+                allowedFields.includes(k)
+            )
+        );
+
+        const id = uuidv4();
+        newProject.id = id;
+
+        console.log(`service.addProject >`, arguments);
+        const added = await Project.create({ newProject });
+        console.log(`service.addProject > added=${added}`);
+        return added;
+    }
+
+    /** Find the first project that exactly matches id/title.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {uuid} [payload.project_id]
+     * @param {String} [payload.title] - Must be an exact match.
+     * @returns {project|null} found - If not found, just null.
+     *  It will not emit error message.
+     *
+     * Both `id` and `title` are optional, but one of them must be provided.
+     * Or we'll be oh so confused that we'll bail out.
+     */
+    static async getProject({ project_id, title }) {
+        console.log(`service.getAward > `, arguments[0]);
+        let found = null;
+        if (project_id) {
+            found = await Project.findById({ project_id });
+        } else if (title) {
+            found = await Project.findByName({ title });
+        }
+
+        return found;
+    }
+
+    /** Find every last project there is.
+     *
+     * @static
+     * @async
+     * @returns {project[]} found - If none, return an empty Array.
+     *  It will not emit error message.
+     */
+    static async getAllProjects() {
+        const found = await Project.findAll();
+        return found;
+    }
+
+    /** Find all projects given to the user.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {uuid} payload.participant_id
+     * @returns {project[]} found - If none, return an empty Array.
+     *  It will not emit error message.
+     */
+    static async getUserProjects({ participant_id }) {
+        // ???
+        console.log(`service.getUserAwards > `, arguments[0]);
+        const found = await Project.searchByAwardee({ participant_id });
+        return found;
+    }
+
+    /** Search projects with title/desc keywords.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {String} [payload.title]
+     * @param {String} [payload.description]
+     * @returns {project[]} found, or an empty Array.
+     *
+     * @todo We're not implementing this until regex escaping can be done.
+     */
+    static async searchProjects({ title, description }) {
+        return [];
+    }
+
+    /** Modify a project.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {uuid} payload.project_id
+     * @param {String[][]} payload.pairs - Array of [key, value] pairs.
+     * @returns {project} updated
+     *
+     * payload.pairs is an iterable of [key, value] pairs.
+     * key will be the field and value will be the... value!!
+     */
+    static async setProject({ project_id, pairs }) {
+        console.log(`service.setProject > `, arguments[0]);
+
+        const allowedFields = [
+            "participant_id",
+            "title",
+            "from_date",
+            "to_date",
+            "description",
+        ];
+        const updated = await Project.update({
+            project_id,
+            pairs: pairs.filter(([k, v]) => {
+                return allowedFields.includes(k);
+            }),
+        });
+
+        return updated;
+    }
+
+    /** Remove a project.
+     *
+     * @static
+     * @async
+     * @param {Object} payload
+     * @param {uuid} payload.project_id
+     * @returns {project} removed
+     */
+    static async removeProject({ project_id }) {
+        console.log(`service.removeAward > `, arguments[0]);
+        const removed = await Project.delete({ project_id });
+        console.log(`removed: `, removed);
+        return removed;
+    }
+}
 
 export { projectService };
