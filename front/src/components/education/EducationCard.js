2fcb5bd68b09d2310b2779cafd108f2afa6ab60d
[4기 - 2반] 윤여건 <kearnyyoon@gmail.com>
2022-03-19 06:02:36 +0000

    Revert "Merge branch 'education-mvp-front' into 'dev'"
    This reverts merge request !17

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,36 +1,37 @@
-import React, { useContext } from "react";
-import { Col, Row } from "react-bootstrap";
-import { EditButton, DeleteButton } from "../common/Button";
-import { EditTableContext, EducationContext } from "../common/context/Context";
+import { Card, Row, Button, Col } from "react-bootstrap";
 
-/** 수상 이력 목록 컴포넌트입니다.
- *
- * @param {boolean} setIsEditing - 편집중 상태 변경 state
- * @returns EducationList and edit button
+/**
+ * @param {Object} val - eduList 배열 안에 있는 각각의 객체, 예시:{ school: "서울대학교", major: "컴퓨터", position: "박사졸업" }
+ * @param {Boolean} isEditable - 편집 가능한지 여부(여기선 자신의 내용이면 편집버튼을 구현하기 위해 가져옴)
+ * @param {Boolean} setIsEditing - 편집 가능한지 여부(여기선 편집 버튼을 누르면 편집이 가능한 상태(편집이 가능한 페이지)로 만들기위해 가져옴)
+ * @returns (\<EducationCard val={val} isEditable={isEditable} setIsEditing={setIsEditing}></EducationCard>)
  */
-function EducationCard({ setIsEditing }) {
-  const isEditable = useContext(EditTableContext);
-  const { school, major, position } = useContext(EducationContext);
+function EducationCard({ val, isEditable, setIsEditing }) {
   return (
-    <Row className="align-items-center">
-      <Col className="mb-3">
-        <span>{school}</span>
-        <br />
-        <span style={{ color: "gray" }}>{major}</span>
-        <span style={{ color: "gray", marginLeft: 5 }}>{position}</span>
-        <br />
-      </Col>
-      {isEditable && (
-        <>
-          <Col sm={1}>
-            <EditButton setState={setIsEditing}>편집</EditButton>
-          </Col>
-          <Col sm={1}>
-            <DeleteButton handleDelete={() => {}} />
-          </Col>
-        </>
-      )}
-    </Row>
+    <div>
+      <Row>
+        <Col xs={11}>
+          {val.school}
+          <Card.Subtitle className="mb-2 text-muted">
+            {val.major}
+            <span style={{ display: "inline-block", margin: 3 }}>
+              ({val.position})
+            </span>
+          </Card.Subtitle>
+        </Col>
+        <Col className="mt-3">
+          {isEditable && (
+            <Button
+              variant="outline-info"
+              size="sm"
+              onClick={() => setIsEditing(true)}
+            >
+              편집
+            </Button>
+          )}
+        </Col>
+      </Row>
+    </div>
   );
 }
 
