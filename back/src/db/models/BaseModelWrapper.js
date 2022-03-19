49451b68b8341c10bb58d065534d60dbb4d0dc82
YunSeongJun <movomo@gmail.com>
2022-03-19 18:56:05 +0900

    Add BaseModelWrapper.js; Class outline and typedef's

---
+++ back/src/db/models/BaseModelWrapper.js
@@ -0,0 +1,28 @@
+/**
+ * @typedef {any} Model - Mongoose model.
+ * @typedef {[string, any]} kvpair - Key, value pair.
+ * @typedef {kvpair[]} kvpairs - Iterable of key, value pairs.
+ */
+
+/** Base class for Models. Implements simple CRUD interface.
+ *
+ * @property {Model} Model
+ * @method static async create(fields) {}
+ * @method static async find(kvpairs) {}
+ * @method static async findAll(kvpairs) {}
+ * @method static async search({ op, regex, kvpairs }) {}
+ * @method static async update({ id, kvpairs }) {}
+ * @method static async delete({ id }) {}
+ */
+class BaseModel {
+    static Model;
+
+    static async create(fields) {}
+    static async find(kvpairs) {}
+    static async findAll(kvpairs) {}
+    static async search({ op, regex, kvpairs }) {}
+    static async update({ id, kvpairs }) {}
+    static async delete({ id }) {}
+}
+
+export { BaseModel };
