f882255a8470405ea4abead083045d7869c5ba59
YunSeongJun <movomo@gmail.com>
2022-03-20 22:33:07 +0900

    extend BaseService > SubrecordService

--- back/src/services/BaseService.js
+++ back/src/services/BaseService.js
@@ -10,14 +10,34 @@ import { BaseModel } from "../db";
  */
 
 /** Base class for Services. Mainly verifies the data fields passing through.
- * @todo check unique in [ ] add, [ ] set
- * @todo subclass subservice and rename getUserOwned
+ * @todo leave required field verification, check unique up to Router
+ * @todo throw most errors here rather than router; [ ] add [ ] set
  *
+ * ## Methods
+ * @constructor constructor({ logger })
+ * @method static async add(record)
+ *  - Add a record to the user.
+ * @method static async get({ id })
+ *  - Find the first record that exactly matches the id.
+ * @method static async getAll(query)
+ *  - Find every last record there is (that matches the optional query).
+ * @method static async set({ id, ...record })
+ *  - Modify a record.
+ * @method static async del({ id })
+ *  - Remove a project.
+ *
+ * ## Static properties
  * @prop {BaseModel} Model
- * @prop {string} name
- * @prop {boolean} deletable - If true, this Service allows deletion of records.
- * @prop {field[]} requiredFields - Required field names.
- * @prop {field[]} optionalFields - Optional field names.
+ * @prop {string} name_
+ *  - The record kind's general name.
+ *    The trailing underscore is to avoid name clash with the automatic
+ *    read-only property 'name' that exists in classes or functions.
+ * @prop {boolean} deletable
+ *  - If true, this Service allows deletion of records.
+ * @prop {field[]} requiredFields
+ *  - Required field names.
+ * @prop {field[]} optionalFields
+ *  - Optional field names.
  * @prop {field[]} settableFields
  *  - Field names that are allowed to be modified.
  * @prop {field[]} uniqueFields
@@ -28,24 +48,18 @@ import { BaseModel } from "../db";
  *  - Indicates that KEY fields need to be populated with VALUEs.
  * @prop {field[]} allFields - All existing fields, only these are allowed in.
  *
- * @method static async add(record) - Add a record to the user.
- * @method static async get({ id })
- *  - Find the first record that exactly matches the id.
- * @method static async getAll(query)
- *  - Find every last record there is (that matches the optional query).
- * @method static async getUserOwned({ user_id })
- *  - Find all records that belong to the user.
- * @method static async set({ id, ...record }) - Modify a record.
- * @method static async del({ id }) - Remove a project.
+ * ## Properties
+ * @prop {Logger} logger
+ *  - Instance of logger; Defaults to console.
  */
 class BaseService {
     static Model = BaseModel;
 
-    static name = "base";
+    static name_ = "base";
     // #_user_id_amend silently amends the mistake in Award, Project schemas
     // whose owner field names are set to some overcomplicated names instead of
     // just 'user_id'.
-    static #_user_id_amend = null;
+    // static #_user_id_amend = null;
     static deletable = true;
 
     static requiredFields = Object.freeze([]);
@@ -61,6 +75,18 @@ class BaseService {
         ...this.optionalFields,
     ]);
 
+    logger;
+
+    /**
+     *
+     * @class
+     * @param {Logger} logger
+     *  - logger instance to use. Defaults to console.
+     */
+    constructor({ logger }) {
+        this.logger = logger ?? console;
+    }
+
     /** Add a record to the user.
      *
      * @static
@@ -69,7 +95,7 @@ class BaseService {
      * @returns {record} added
      */
     static async add(record) {
-        console.log(`${this.name}.add >`, arguments[0]);
+        console.log(`${this.name_}.add >`, arguments[0]);
 
         // Squash unnecessary fields first.
         const data = Object.fromEntries(
@@ -83,7 +109,7 @@ class BaseService {
         this.requiredFields.forEach((k) => {
             if (!(k in data)) {
                 throw new Error(
-                    `${k} field is required for ${this.name} record`
+                    `${k} field is required for ${this.name_} record`
                 );
             }
         });
@@ -92,7 +118,7 @@ class BaseService {
         data.id = uuidv4();
 
         const added = await this.Model.create(data);
-        console.log(`${this.name}.add > added=`, added);
+        console.log(`${this.name_}.add > added=`, added);
 
         return added;
     }
@@ -106,7 +132,7 @@ class BaseService {
      *      It will not emit error message.
      */
     static async get({ id }) {
-        console.log(`${this.name}Service.get > `, arguments[0]);
+        console.log(`${this.name_}Service.get > `, arguments[0]);
 
         // Confirmed, null query is ok.
         // Probably because we don't have any null in the db.
@@ -130,7 +156,7 @@ class BaseService {
      *      It will not emit error message.
      */
     static async getAll(query) {
-        console.log(`${this.name}Service.getAll > `, arguments[0]);
+        console.log(`${this.name_}Service.getAll > `, arguments[0]);
 
         // It's ok to omit query!
         if (!query) {
@@ -140,30 +166,12 @@ class BaseService {
         return found;
     }
 
-    /** Find all records that belong to the user.
+    /** This is a placeholder to get populated? not? child refs.
      *
-     * @static
-     * @async
-     * @param {{user_id: uuid}} payload
-     * @returns {project[]} found - If none, return an empty Array.
-     *  It will not emit error message.
+     * @param {field} path
+     * @returns {record|record[]} children
      */
-    static async getUserOwned({ user_id }) {
-        console.log(`${this.name}Service.getUserOwned > `, arguments[0]);
-
-        const query = {};
-        if (this.#_user_id_amend) {
-            // Passing this test means that at some point of the past I
-            // fucked up hard and now facing the consequences.
-            query[this.#_user_id_amend] = user_id;
-        } else {
-            query.user_id = user_id;
-        }
-
-        const found = await this.Model.findAll(query);
-
-        return found;
-    }
+    static async getChildren({ path }) {}
 
     /** Modify a record.
      *
@@ -173,7 +181,7 @@ class BaseService {
      * @returns {record|null} updated
      */
     static async set({ id, ...record }) {
-        console.log(`${this.name}Service.set > `, arguments[0]);
+        console.log(`${this.name_}Service.set > `, arguments[0]);
 
         // We're picking out uneditable entries here.
         const toUpdate = Object.fromEntries(
@@ -197,14 +205,72 @@ class BaseService {
      * It is an error to attempt to delete an undeletable record.
      */
     static async del({ id }) {
-        console.log(`${this.name}.del > `, arguments[0]);
+        console.log(`${this.name_}.del > `, arguments[0]);
         if (this.deletable) {
             const removed = await this.Model.delete({ id });
 
             return removed;
         } else {
-            throw new Error(`${this.name} is not deletable`);
+            throw new Error(`${this.name_} is not deletable`);
         }
+    }
+}
+
+/** Service template for record kind that can be a child of another record.
+ *
+ * @extends BaseService
+ *
+ * ## Methods
+ * @method static async getParent({ id, ...query })
+ *  - Find and return the *parent* of the found record.
+ * @method static async getSiblings({ user_id })
+ *  - Find all records that belong to the owner, who isn't necessarily user.
+ */
+class SubrecordService extends BaseService {
+    static ownerField = "user_id";
+
+    /** Find and return the *parent* of the found record.
+     *
+     * @param {{id: uuid, query: record}} payload
+     *  - Aside from id, optional queries may be provided.
+     * @returns {record|null} parent
+     *  - `null`: The record itself exists and it just has no parent.
+     *  - Error: The record was not found.
+     */
+    static async getParent({ id, ...query }) {
+        console.log(`${this.name_}Service.getParent > `, arguments[0]);
+
+        const me = await this.Model.find({ id, ...query });
+        if (!me) {
+            throw new Error(
+                `${this.name_} with query ${artuments[0]} not found`
+            );
+        }
+
+        const parent = await this.Model.find({
+            [this.ownerField]: me[this.ownerField],
+        });
+
+        return parent;
+    }
+
+    /** Find all records that belong to the owner.
+     *
+     * @static
+     * @async
+     * @param {{user_id: uuid}} payload
+     *  - The user_id field is named so because it's used a lot,
+     *    but theoretically it can be any.
+     * @returns {record[]} found
+     *  - If none, return an empty Array. It will not emit error message.
+     */
+    static async getSiblings({ user_id }) {
+        console.log(`${this.name_}Service.getSiblings > `, arguments[0]);
+
+        const query = { [this.ownerField]: user_id };
+        const found = await this.Model.findAll(query);
+
+        return found;
     }
 }
 
