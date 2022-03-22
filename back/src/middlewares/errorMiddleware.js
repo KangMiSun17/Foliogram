fc78d125e639b75a05a57bcda7421edfbae71504
YunSeongJun <movomo@gmail.com>
2022-03-23 00:59:13 +0900

    remove color controllers in errorMiddleware because it wont work with logger

--- back/src/middlewares/errorMiddleware.js
+++ back/src/middlewares/errorMiddleware.js
@@ -11,7 +11,7 @@ const logger = new Logger({
 
 function errorMiddleware(error, req, res, next) {
     // console.log("\x1b[33m%s\x1b[0m", error);
-    logger.log({ __level__: 1 }, "\x1b[33m%s\x1b[0m", error);
+    logger.log({ __level__: 1 }, error);
 
     // res.status(400).send(error.message);
     if (error instanceof RequestError) {
