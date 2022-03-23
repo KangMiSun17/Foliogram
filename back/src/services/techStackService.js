1e3d6aa1cb8ac60a590cc16f7f4870a4aea9a7d6
YunSeongJun <movomo@gmail.com>
2022-03-23 23:08:37 +0900

    implement techstack mvp

---
+++ back/src/services/techStackService.js
@@ -0,0 +1,56 @@
+import { TeckStack } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import { SubrecordService } from "./BaseService";
+import { Logger, UNIFIED_LOG } from "../utils/logging";
+
+class TeckStackService extends SubrecordService {
+    // Inherit from BaseService
+    Model = TeckStack;
+
+    // deletable = true;
+
+    requiredFields = Object.freeze(["id", "user_id", "title"]);
+    optionalFields = Object.freeze(["description"]);
+    settableFields = Object.freeze(["title", "description"]);
+    // uniqueFields = Object.freeze([]);
+    // searchableFields = Object.freeze([]);
+
+    // authField = "user_id";
+    // refFields = Object.freeze({});
+
+    allFields = Object.freeze([...this.requiredFields, ...this.optionalFields]);
+
+    // logger;
+
+    // Inherit from SubrecordService
+    // ownerField = "user_id";
+
+    // Here we override some methods to reduce degree of freedom.
+    // Too many choices can be bad if the routers get funny ideas.
+    // Freedom is very expensive indeed.
+
+    async get({ id }) {
+        return super.get({ id });
+    }
+
+    async del({ id, currentUserId }) {
+        return super.del({ id, currentUserId });
+    }
+
+    async getParent({ id }) {
+        return super.getParent({ id });
+    }
+}
+
+const logger = new Logger({
+    name: "teckStackService",
+    tee: [
+        UNIFIED_LOG,
+        Logger.generateLogPath("teckstack.log"),
+        Logger.generateLogPath("service.log"),
+        Logger.generateLogPath("teckstackservice.log"),
+    ],
+});
+
+const teckStackService = new TeckStackService({ logger });
+
+export { teckStackService };
