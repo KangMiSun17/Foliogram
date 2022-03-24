7a471456c5a2c22aa1cf7d8883a9623d84c30306
ms <altjsvnf@naver.com>
2022-03-25 00:19:16 +0900

    eslint error fix

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -49,6 +49,7 @@ function Network() {
                 </Col>
               );
             }
+            return null;
           })}
         </Row>
       )}
@@ -62,6 +63,7 @@ function Network() {
                 </Col>
               );
             }
+            return null;
           })}
         </Row>
       )}
@@ -75,6 +77,7 @@ function Network() {
                 </Col>
               );
             }
+            return null;
           })}
         </Row>
       )}
