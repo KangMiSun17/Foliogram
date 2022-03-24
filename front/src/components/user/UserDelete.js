9d9a6416dcf1b7b98ecd89eb6756bc44b66658e9
handwoong <handwoong@gmail.com>
2022-03-25 02:43:47 +0900

    Delete user API test

--- front/src/components/user/UserDelete.js
+++ front/src/components/user/UserDelete.js
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import { OwnerContext } from "./../common/context/Context";
+import { useNavigate } from "react-router-dom";
+import { UserStateContext } from "./../../App";
+import * as Api from "../../api";
 
 function UserDelete() {
   const { isEditable } = useContext(OwnerContext);
+  const userState = useContext(UserStateContext);
+  const navigate = useNavigate();
 
   const handleUserDelete = (e) => {
     e.preventDefault();
+
+    const deleteUser = async () => {
+      try {
+        await Api.delete("users", userState.user.id);
+        navigate("/login");
+      } catch (err) {
+        alert("탈퇴에 실패하였습니다.", err);
+      }
+    };
+
     const userMessage = prompt(
       '탈퇴를 하시려면 "탈퇴하겠습니다"를 입력해주세요.'
     );
@@ -16,17 +31,12 @@ function UserDelete() {
     }
 
     const confirmMessage = window.confirm("회원 탈퇴를 진행합니다.");
-    alert(confirmMessage);
 
-    if (confirmMessage) {
-      // 탈퇴 요청 API
-      try {
-        console.log("탈퇴 성공");
-        return;
-      } catch (err) {
-        console.log("탈퇴에 실패하였습니다.".err);
-      }
+    if (!confirmMessage) {
+      return;
     }
+
+    deleteUser();
   };
 
   return (
