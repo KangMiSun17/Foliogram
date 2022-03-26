65c6d519f64b260fecda0119a1b33dacdc59d475
kunnie-code <kearnyyoon@gmail.com>
2022-03-26 21:38:46 +0900

    show 전체보기

--- front/src/components/nav/NavBar.js
+++ front/src/components/nav/NavBar.js
@@ -130,7 +130,7 @@ function NavBar({ navList, setNavList, setTogglePage }) {
           return null;
         })}
 
-        {isEditable && (
+        {isEditable ? (
           <>
             <Button className="ms-auto" variant="light" onClick={handleShowAll}>
               전체보기
@@ -186,6 +186,13 @@ function NavBar({ navList, setNavList, setTogglePage }) {
               </Dropdown.Menu>
             </Dropdown>
             {/* <Button style={{ marginRight: 20 }}>저장</Button> */}
+          </>
+        ) : (
+          <>
+            <Button className="ms-auto" variant="light" onClick={handleShowAll}>
+              전체보기
+            </Button>
+            <div style={{ marginRight: "12px" }}></div>
           </>
         )}
       </Stack>
