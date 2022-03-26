10ddbb9a43c0eb48605ad495068939db223ad3bb
handwoong <handwoong@gmail.com>
2022-03-26 23:51:34 +0900

    로그인 실패 alert 추가

--- front/src/components/user/User.js
+++ front/src/components/user/User.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import UserEditForm from "./UserEditForm";
 import UserCard from "./UserCard";
 import ProfileImage from "./ProfileImage";
