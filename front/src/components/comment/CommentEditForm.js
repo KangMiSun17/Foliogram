3ff632b509b198b5e11cda329cb0ff1cdc1997c8
ms <altjsvnf@naver.com>
2022-03-26 14:32:55 +0900

    comment code refectoring

--- front/src/components/comment/CommentEditForm.js
+++ front/src/components/comment/CommentEditForm.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import { BundleButton } from "../common/Button";
 import * as Api from "../../api";
 import { CommentFetchContext } from "../common/context/Context";
@@ -34,36 +34,23 @@ function CommentEditForm({ comment, setIsEditing, index }) {
   };
 
   return (
-    <Row className="align-items-center">
-      <Col sm={1} className="ms-1">
-        <img
-          src={comment.user_id.profileImage}
-          style={{
-            width: "32px",
-            height: "32px",
-            marginLeft: "5px",
-            borderRadius: "13px",
-            border: "2px gray solid",
-          }}
-          alt="comment img"
-        />
-      </Col>
-      <Col className="ms-3 mt-3 mb-3">
-        <span>{comment.user_id.name}님</span>
-      </Col>
-      <Row className="ms-2 mb-2">
-        <br />
-        <textarea
+    <div>
+      <Row className="justify-content-center">
+        <Form.Control
+          className="mb-2"
+          as="textarea"
+          placeholder="댓글을 입력해주세요"
+          style={{ height: "100px", width: "80%" }}
           value={editContent}
           onChange={(e) => setEditContent(e.target.value)}
-        ></textarea>
+        />
       </Row>
       <BundleButton
         disabled={editContent.length === 0}
         setState={setIsEditing}
         submitHandler={submitHandler}
       />
-    </Row>
+    </div>
   );
 }
 
