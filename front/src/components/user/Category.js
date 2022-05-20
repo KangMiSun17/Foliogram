1cc7a5ca6f895e51c37b5d1eb00765ea90cd7d9e
ms <altjsvnf@naver.com>
2022-05-20 23:12:49 +0900

    edit: category code

--- front/src/components/user/Category.js
+++ front/src/components/user/Category.js
@@ -13,15 +13,17 @@ function Category({ setCategory }) {
       title="카테고리 선택"
       className="mb-3"
     >
-      <Dropdown.Item onClick={() => setCategory("all")}>전체</Dropdown.Item>
+      <Dropdown.Item onClick={() => setCategory("전체")}>전체</Dropdown.Item>
       <Dropdown.Divider />
-      <Dropdown.Item onClick={() => setCategory("developer")}>
+      <Dropdown.Item onClick={() => setCategory("개발자")}>
         개발자
       </Dropdown.Item>
-      <Dropdown.Item onClick={() => setCategory("artist")}>
+      <Dropdown.Item onClick={() => setCategory("예술가")}>
         예술가
       </Dropdown.Item>
-      <Dropdown.Item onClick={() => setCategory("mafia")}>마피아</Dropdown.Item>
+      <Dropdown.Item onClick={() => setCategory("마피아")}>
+        마피아
+      </Dropdown.Item>
     </DropdownButton>
   );
 }
